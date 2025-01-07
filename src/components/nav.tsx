3874f63867c88f76f5e794403a545f9357a65662
Biggie Cheese <Jv3studios@gmail.com>
2025-01-07 12:00:49 -0600

    Fixed up Nav styles

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,4 +1,5 @@
 import "../styles/nav.scss";
+import { useState, useEffect } from "react";
 
 export const MobileNav = () => {
   return (
@@ -14,38 +15,70 @@ export const MobileNav = () => {
   );
 };
 
+export const NavLinks = () => {
+  return (
+    <div className="nav-wrapper-left">
+      <figure>
+        <h1>SP</h1>
+      </figure>
+      <ul>
+        <li>
+          <a href="#">Courses</a>
+        </li>
+        <li>
+          <a href="#">Resources</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
 export const Nav = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Monitor the window size
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <nav className="nav">
-      <div className="nav-wrapper-left">
-        <figure>
-          <h1>SP</h1>
-        </figure>
-        <ul>
-          <li>
-            <a href="#">Courses</a>
-          </li>
-          <li>
-            <a href="#">Resources</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Blog</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </div>
+      {isMobile ? (
+        <div className="nav-wrapper-left">
+          <button>
+            <i className="bx bx-menu"></i>
+          </button>
+        </div>
+      ) : (
+        <NavLinks />
+      )}
       <div className="nav-wrapper-right">
         <div>
-          <button>Sign Up</button>
+          <button className="sign-up">
+            <i className="bx bx-user"></i>Sign Up
+          </button>
         </div>
         <div>
-          <i className="bx bx-log-in"></i>
-          <button>Log In</button>
+          <button className="log-in">
+            <i className="bx bx-log-in"></i>Log In
+          </button>
         </div>
       </div>
     </nav>
