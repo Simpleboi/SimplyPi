42ff1cba3b76c2e50bfe7f0cd6311143337ce24f
Biggie Cheese <Jv3studios@gmail.com>
2025-01-31 11:56:24 -0600

    working on alg1 course

--- src/pages/courses/algebra1.tsx
+++ src/pages/courses/algebra1.tsx
@@ -1,5 +1,24 @@
 import React from "react"
+import "../../styles/courses/algebra1.scss"
+
+export const Alg1ContentOfTables = () => {
+    return(
+    <section className="alg1-toc">
+        
+        <details>
+            <summary>Introduction to Algebra 1</summary>
+            Introduction to Algebra 1
+        </details>
+    </section>
+)
+}
+
 
 export const Alg1 = () => {
-    return(<h1>Hello World</h1>)
+    return(
+    <section className="algebra-1">
+        <Alg1ContentOfTables />
+        <h2>Hello World! As an example</h2>
+    </section>
+)
 }
